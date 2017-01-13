85eb39b53e88b1d7b1ab54b86e8868929145282b
Kevin Charles <rossgithub@gmail.com>
2017-01-13 19:52:20 +0000

    Added Section for reading the file line by line
    Implemented the section of the code that reads the file line by line,
    a csv reader is used to read the file and break it down into records.
    
    -created new type ImportRecord
    -added test file for main.go file
    -added test for NewImportItem function

--- regex_validator/main.go
+++ regex_validator/main.go
@@ -1,28 +1,58 @@
 package main
 
 import (
+	"bufio"
+	"encoding/csv"
 	"flag"
 	"fmt"
+	"io"
 	"os"
 	"path/filepath"
+	"strconv"
 )
 
-func errorExit(str string, code int) {
-	fmt.Fprintf(os.Stderr, "%s\n", str)
-	os.Exit(code)
+// type to represet a record from an imported .csv file , rowId & postcodes or the record is stored in
+// their native types rather than both being stored as strings
+type ImportRecord struct {
+	rowId         uint32
+	postcode      string
+	beenValidated bool
+	isValid       bool
 }
 
-func check(e error) {
-	if e != nil {
-		panic(e)
+// takes a record read by a cvs reader (which creates a string slice of each record) and creates a properly
+// typed ImportRecord from this slice
+func NewImportItem(record []string) *ImportRecord {
+	if len(record) > FIELDS_PER_RECORD || len(record) < FIELDS_PER_RECORD {
+		panic("invalid record received")
 	}
+
+	const ROW_ID_IDX = 0
+	const POSTCODE_IDX = 1
+
+	rowIdInt, err := strconv.ParseInt(record[ROW_ID_IDX], 10, 32)
+	check(err)
+
+	return &ImportRecord{rowId: uint32(rowIdInt),
+		postcode:      record[POSTCODE_IDX],
+		beenValidated: false,
+		isValid:       false}
 }
+
+const (
+	BATCH_SIZE_DEFAULT int = 10000
+	FIELDS_PER_RECORD  int = 2
+)
 
 func main() {
 
 	// get the file name sent in via the command line flag ------------------------------------------------
 	var path string
 	flag.StringVar(&path, "file", "", "the location of the .csv file")
+
+	var batchSize int
+	flag.IntVar(&batchSize, "batch-size", BATCH_SIZE_DEFAULT, "how many records the program will process at one time")
+
 	flag.Parse()
 
 	if len(path) == 0 {
@@ -42,6 +72,7 @@ func main() {
 	csvFile, err := os.Open(path)
 	check(err)
 
+	// defer the closing of the file until the end of main()
 	defer func() {
 		err := csvFile.Close()
 		check(err)
@@ -49,7 +80,32 @@ func main() {
 
 	// batch read the file line by line (Read 10,000 at a time, and fill up 10k structs) ------------------
 
-	// seperate the lines based on thier delimiter --------------------------------------------------------
+	// create a reader that can read csv files , init the readers values
+	csvReader := csv.NewReader(csvFile)
+	csvReader.FieldsPerRecord = FIELDS_PER_RECORD
+
+	// make slices to hold the valid and invalid ImportRecords
+	//validImportRecs := make([]*ImportRecord, 0)
+	//invalidImportRecs := make([]*ImportRecord, 0)
+
+	// keep processing in batches
+	completed := false
+	for !completed {
+
+		for i := 0; i < batchSize; i++ {
+			currRecord, err := csvReader.Read()
+
+			// reading is completed once we reach the end of the file
+			if err == io.EOF {
+				completed = true
+				break
+			}
+
+			fmt.Println(currRecord)
+
+		}
+		//completed = true
+	}
 
 	// put seperated values into a struct that stores the POSTCODE & ROW ID -------------------------------
 
@@ -61,5 +117,17 @@ func main() {
 
 	// do this for another 10k items until completion -----------------------------------------------------
 
-	fmt.Println("reached end successfully")
+	fmt.Print("Press 'Enter' to continue...")
+	bufio.NewReader(os.Stdin).ReadBytes('\n')
+}
+
+func errorExit(str string, code int) {
+	fmt.Fprintf(os.Stderr, "%s\n", str)
+	os.Exit(code)
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
 }
---
+++ regex_validator/main_test.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"testing"
+)
+
+func Test_IsStringValid__NewImportItem(t *testing.T) {
+
+	expected := true
+	result := false
+
+	testRecords := [][]string{[]string{"1064397", "MK12 5EY"},
+		[]string{"1995262", "W6 8EX"},
+		[]string{"803671", "IP20 9DL"},
+		[]string{"1779862", "SW12 0EF"},
+		[]string{"1434686", "RG10 8AU"},
+		[]string{"1508688", "RM20 4AP"},
+		[]string{"472843", "DH4 7DU"}}
+
+	for _, element := range testRecords {
+		item := NewImportItem(element)
+
+		num, _ := strconv.Atoi(element[0])
+		result = item.beenValidated == false && item.isValid == false && item.postcode == element[1] && item.rowId == uint32(num)
+
+		if result != expected {
+			error := fmt.Sprintf("Given NewImportItem, Expected: %t   got: %t", expected, result)
+			t.Error(error)
+		}
+	}
+
+}
