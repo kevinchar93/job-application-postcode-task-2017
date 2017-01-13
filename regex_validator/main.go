a8df1f4b6fa9ad90777967975c3c4848e0b91436
Kevin Charles <rossgithub@gmail.com>
2017-01-13 14:29:29 +0000

    Implement file name input
    Added code to take the name of the .csv file to process.
    Name is taken as a command line flag and is checked to see if it exists
    before letting the program continue

--- regex_validator/main.go
+++ regex_validator/main.go
@@ -1,29 +1,54 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
+	"path/filepath"
 )
 
+func errorExit(str string, code int) {
+	fmt.Fprintf(os.Stderr, "%s\n", str)
+	os.Exit(code)
+}
+
 func main() {
-	fmt.Println("Hello world!")
 
-	// get the file name sent in via the command line flag
+	// get the file name sent in via the command line flag ------------------------------------------------
+	var path string
+	flag.StringVar(&path, "file", "", "the location of the .csv file")
+	flag.Parse()
 
-	// use the file name to find the file and open it
+	if len(path) == 0 {
+		errorExit("No path to or name of a .csv file was provided", 1)
+	}
 
-	// OPTIONAL: untar the file if there exists a library to do so
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		errStr := fmt.Sprintf("The file or path provided does not exist: \"%s\"", path)
+		errorExit(errStr, 1)
+	}
 
-	// batch read the file line by line (Read 10,000 at a time, and fill up 10k structs)
+	if filepath.Ext(path) != ".csv" {
+		errorExit("File must have the extension .csv", 1)
+	}
 
-	// seperate the lines based on thier delimiter
+	// use the file name to find the file and open it -----------------------------------------------------
 
-	// put seperated values into a struct that stores the POSTCODE & ROW ID
+	// OPTIONAL: untar the file if there exists a library to do so ----------------------------------------
 
-	// iterate over collection of postcode structs an check validity of each using regex
+	// batch read the file line by line (Read 10,000 at a time, and fill up 10k structs) ------------------
 
-	// put them into seperate collections depending on validity
+	// seperate the lines based on thier delimiter --------------------------------------------------------
 
-	// write each collection to a CSV file
+	// put seperated values into a struct that stores the POSTCODE & ROW ID -------------------------------
 
-	// do this for another 10k items until completion
+	// iterate over collection of postcode structs an check validity of each using regex ------------------
+
+	// put them into seperate collections depending on validity -------------------------------------------
+
+	// write each collection to a CSV file ----------------------------------------------------------------
+
+	// do this for another 10k items until completion -----------------------------------------------------
+
+	fmt.Println("reached end successfully")
 }
